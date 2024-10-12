c7dedf444e911254644345aafefc338232670576
Jose <jgironfo@emeal.nttdata.com>
2024-10-12 13:43:23 +0200

    Añadiendo segundo test

--- src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
@@ -8,4 +8,9 @@ public class BowlingGameTest {
     public void jessicaTestOne() {
     System.out.println("TestOne");
     }
+
+    @Test
+    public void jessicaTestTwo() {
+        System.out.println("TestTwo");
+    }
 }
